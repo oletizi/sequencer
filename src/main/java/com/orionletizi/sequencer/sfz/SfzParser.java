66dbe6551361237872f0ecdf69963b5e77cf1560
Orion Letizi <oletizi@mac.com>
2015-11-16 15:22:33 -0800

    Made the parser a bit more robust. It now tolerates multiple key/value pairs on the same line

--- src/main/java/com/orionletizi/sequencer/sfz/SfzParser.java
+++ src/main/java/com/orionletizi/sequencer/sfz/SfzParser.java
@@ -31,32 +31,68 @@ public class SfzParser {
   public void parse(final InputStream inputStream) throws IOException {
     final BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
     String line = null;
-    // TODO: This parser isn't very flexible. It should handle whitespace and comments and probably other stuff
     while ((line = in.readLine()) != null) {
-      if (line.startsWith("<group>")) {
+      line = stripLeadingWhitespace(line);
+      if (line.startsWith("//")) {
+        continue;
+      } else if (line.startsWith("<group>")) {
         state = States.group;
         observer.notifyGroup();
-        line = line.substring("<group>".length());
+        line = stripLeadingWhitespace(line.substring("<group>".length()));
       } else if (line.startsWith("<region>")) {
         state = States.region;
         observer.notifyRegion();
-        line = line.substring("<region>".length());
+        line = stripLeadingWhitespace(line.substring("<region>".length()));
       }
 
-      if (line.startsWith("sample=")) {
-        line = line.substring("sample=".length());
-        observer.notifySample(line);
-      } else if (line.startsWith("lokey=")) {
-        line = line.substring("lokey=".length());
-        observer.notifyLokey(line);
-      } else if (line.startsWith("hikey=")) {
-        line = line.substring("hikey=".length());
-        observer.notifyHikey(line);
-      } else if (line.startsWith("pitch_keycenter=")) {
-        line = line.substring("pitch_keycenter=".length());
-        observer.notifyPitchKeycenter(line);
+      while (!"".equals(line)) {
+        info("line: " + line);
+        if (line.startsWith("sample=")) {
+          line = line.substring("sample=".length());
+          final String sample = nextToken(line);//line.substring(0, line.indexOf(' '));
+          observer.notifySample(sample);
+          line = shift(sample, line);
+        } else if (line.startsWith("lokey=")) {
+          line = line.substring("lokey=".length());
+          final String lokey = nextToken(line);
+          observer.notifyLokey(lokey);
+          line = shift(lokey, line);
+        } else if (line.startsWith("hikey=")) {
+          line = line.substring("hikey=".length());
+          final String hikey = nextToken(line);
+          observer.notifyHikey(hikey);
+          line = shift(hikey, line);
+        } else if (line.startsWith("pitch_keycenter=")) {
+          line = line.substring("pitch_keycenter=".length());
+          final String keycenter = nextToken(line);
+          observer.notifyPitchKeycenter(keycenter);
+          line = shift(keycenter, line);
+        } else {
+          line = shift(line);
+        }
       }
     }
+  }
+
+  private void info(String s) {
+    System.out.println("INFO: " + s);
+  }
+
+  private String nextToken(String line) {
+    final int nextSpace = line.indexOf(' ');
+    return nextSpace != -1 ? line.substring(0, line.indexOf(' ')) : line;
+  }
+
+  private String shift(String line) {
+    return shift(nextToken(line), line);
+  }
+
+  private String shift(String token, String line) {
+    return stripLeadingWhitespace(line.replaceAll("^" + token, ""));
+  }
+
+  private String stripLeadingWhitespace(String line) {
+    return line.replaceAll("^\\s+", "");
   }
 
 
--- src/test/java/com/orionletizi/sequencer/sfz/SfzParserTest.java
+++ src/test/java/com/orionletizi/sequencer/sfz/SfzParserTest.java
@@ -11,18 +11,20 @@ import static org.mockito.Mockito.*;
 public class SfzParserTest {
 
   private URL sfzResource;
+  private SfzParser parser;
+  private SfzParserObserver observer;
 
   @Before
   public void before() throws Exception {
     sfzResource = ClassLoader.getSystemResource("sfz/mellotron/mk2flute.sfz");
     assertNotNull(sfzResource);
+    parser = new SfzParser();
+    observer = mock(SfzParserObserver.class);
+    parser.addObserver(observer);
   }
 
   @Test
   public void testParse() throws Exception {
-    final SfzParser parser = new SfzParser();
-    final SfzParserObserver observer = mock(SfzParserObserver.class);
-    parser.addObserver(observer);
     parser.parse(sfzResource);
 
     verify(observer, times(1)).notifyGroup();
@@ -33,4 +35,16 @@ public class SfzParserTest {
     verify(observer, times(1)).notifyPitchKeycenter("g2");
   }
 
+  @Test
+  public void testParseWithoutLinebreaks() throws Exception {
+    sfzResource = ClassLoader.getSystemResource("sfz/america2/america2.sfz");
+    assertNotNull(sfzResource);
+
+    parser.parse(sfzResource);
+    verify(observer, times(76)).notifyRegion();
+    verify(observer, times(1)).notifySample("AcousticGuitar-A-section1.wav");
+    verify(observer, times(1)).notifyLokey("24");
+    verify(observer, times(1)).notifyHikey("24");
+    verify(observer, times(1)).notifyPitchKeycenter("24");
+  }
 }
